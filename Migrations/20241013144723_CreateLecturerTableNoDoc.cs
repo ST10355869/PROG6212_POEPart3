0d4a0cd8e03ec8634054e13c44baa18a603f70de
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-10-13 16:49:36 +0200

    updated database to create part2 database and show lecterur table

---
+++ Migrations/20241013144723_CreateLecturerTableNoDoc.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ST10355869_PROG6212_Part2.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLecturerTableNoDoc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lecturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoursWorked = table.Column<double>(type: "float", nullable: false),
+                    HourlyRate = table.Column<double>(type: "float", nullable: false),
+                    AdditionalNotes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lecturers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lecturers");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ST10355869_PROG6212_Part2.Data;
+
 namespace ST10355869_PROG6212_Part2
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ST10355869_PROG6212_Part2
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
