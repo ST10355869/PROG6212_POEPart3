d81cabb47e0e6e34d86eb3015596a65e70f58182
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-10-13 15:30:41 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10355869_PROG6212_Part2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
