4c4645f2a86be13b4b2f8bd11d53b4fd46d5af8d
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-10-13 15:56:46 +0200

    added lecturer model to hold hours,hourly rate and additional notes

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ST10355869_PROG6212_Part2.Models;
+
+namespace ST10355869_PROG6212_Part2.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Lecturer> Lecturers { get; set; }
+
+    }
+}
---
+++ Models/Lecturer.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10355869_PROG6212_Part2.Models
+{
+    public class Lecturer
+    {
+        public int Id { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate {  get; set; }
+        public string AdditionalNotes { get; set; }
+    }
+}
