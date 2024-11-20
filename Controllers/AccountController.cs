eeab3b1f4147fa79704227be77852b6ed6278bd9
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-11-20 15:15:54 +0200

    Added model and controller for use in authentication

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10355869_PROG6212_Part2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AuthenticationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ST10355869_PROG6212_Part2.Models
+
+{
+    public class AuthenticationModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
