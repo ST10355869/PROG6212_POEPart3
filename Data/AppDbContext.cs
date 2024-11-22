31879b517218f9d54d781179e931abc5852fd06f
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-11-22 14:02:16 +0200

    added database seeder to add identities to project

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ST10355869_PROG6212_Part2.Models;
 
 namespace ST10355869_PROG6212_Part2.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<LecturerModel> Lecturers { get; set; }
-
     }
 }
---
+++ Data/DatabaseSeeder.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ST10355869_PROG6212_Part2.Data
+{
+    public class DatabaseSeeder
+    {
+        public static async Task SeedRolesAndUsers(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] roles = { "Admin", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            var adminUser = new IdentityUser
+            {
+                UserName = "manager1",
+                Email = "manager1@example.com"
+            };
+
+            if (userManager.Users.All(u => u.UserName != adminUser.UserName))
+            {
+                var result = await userManager.CreateAsync(adminUser, "Admin@123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+
+            var normalUser = new IdentityUser
+            {
+                UserName = "user",
+                Email = "user@example.com"
+            };
+
+            if (userManager.Users.All(u => u.UserName != normalUser.UserName))
+            {
+                var result = await userManager.CreateAsync(normalUser, "User@123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(normalUser, "User");
+                }
+            }
+        }
+    }
+}
